18467eda374bc90001300aca7f04a29312ec6dc6
yurhzzu <yurhzzu@163.com>
2019-01-02 22:03:38 +0800

    add some banner info

---
+++ src/pages/detail/Detail.vue
@@ -0,0 +1,17 @@
+<template>
+  <Banner></Banner>
+</template>
+
+<script>
+import Banner from './components/Banner'
+export default {
+  name: 'Detail',
+  components: {
+    Banner: Banner
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="banner">
+    <img class="banner-img" src="http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg" alt="">
+    <div class="banner-info">
+      <div class="banner-title">大连圣亚海洋世界（AAA景区）</div>
+      <div class="banner-number"><span class="iconfont">&#xe632;</span>39</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Banner'
+}
+</script>
+
+<style scoped>
+  .banner {
+    position: relative;
+    height: 0;
+    padding-bottom: 55%;
+    overflow: hidden;
+  }
+  .banner-img {
+    width: 100%;
+  }
+  .banner-info {
+    display: flex;
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    line-height: .6rem;
+    color: white;
+    background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8));
+  }
+  .banner-title {
+    flex: 1;
+  }
+  .banner-number {
+    padding: 0 .4rem;
+    height: .4rem;
+    line-height: .4rem;
+    border-radius: .2rem;
+    background: rgba(0, 0, 0, .8);
+  }
+</style>
